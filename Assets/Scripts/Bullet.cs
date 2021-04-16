31016dc4a050dd5d892693a0991bcb7f74e4cb1d
alperenaydas <b21726947@cs.hacettepe.edu.tr>
2021-04-16 22:35:59 +0300

    projectile added

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 40f;
+    public float lifeDuration = 3f;
+    public float damage = 3f;
+    private float lifeTimer;
+    void Start()
+    {
+        lifeTimer = lifeDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+        lifeTimer -= Time.deltaTime;
+        if(lifeTimer<=0f) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectileShooter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileShooter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bullet;
+    public float fireRate = 1f;
+    float nextFire;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && Time.time > nextFire) {
+            Fire();
+            nextFire = Time.time + fireRate;
+        }
+    }
+
+    public void Fire()
+    {
+        Debug.Log("Fire");
+        GameObject projectile = Instantiate(bullet);
+        projectile.transform.position = transform.position + transform.forward;
+        projectile.transform.forward = transform.forward;
+    }
+}
